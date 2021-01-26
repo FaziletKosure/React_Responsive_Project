2754fe7f2dde5a9bddd70cdafa6cc645a9c4629d
FaziletKosure <faziletkosure1@gmail.com>
2021-01-26 18:55:34 +0100

    add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,59 @@
 import React from 'react'
-import styled from "styled-components"
+import styled,{css} from "styled-components/macro"
 import {Link} from "react-router-dom"
+import { menuData } from '../data/MenuData'
 
 
 const Nav=styled.nav`
 height:60px;
 background:blue;
+display:flex;
+justify-content:space-between;
+padding:1rem 2rem;
+z-index:1;
+position:fixed;
+width:100%;
 `
-
-const Logo=styled(Link)`
+const NavLink= css`
 color:#fff;
+align-items:center;
+padding:1rem 1rem;
+height:100%;
+cursor:pointer;
+text-decoration:none;
+`
+const Logo=styled(Link)`
+${NavLink}
+font-style:italic;
 `
 
 const MenuBars=styled.i`
 `
 
-const NavMenu=styled.i`
+const NavMenu=styled.div`
+display:flex;
+align-items:center;
+
+`
+
+const NavMenuLinks=styled(Link)`
+${NavLink}
 `
 const Navbar = () => {
     return (
         <Nav>
-            <Logo>FZLT</Logo>
+            <Logo to='/'>FZLT</Logo>
             <MenuBars/>
             <NavMenu>
+                {menuData.map((item,index)=>(
+                    <NavMenuLinks to={item.link} key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+
+                ))}
 
             </NavMenu>
-           <h1>Navbar</h1> 
+           
         </Nav>
     )
 }
