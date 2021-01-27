4a1b2708c275b5daa8f35180f83ad81e5ae89dde
FaziletKosure <faziletkosure1@gmail.com>
2021-01-27 01:53:46 +0100

    add

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React,{useState,useRef} from 'react'
+import React,{useState,useRef,useEffect} from 'react'
 import styled,{css} from 'styled-components/macro'
 import { Button } from './Button'
 import {IoMdArrowRoundForward} from 'react-icons/io'
@@ -130,14 +130,35 @@ const Hero = ({slides}) => {
     const length=slides.length
     const timeout=useRef(null)
 
+    useEffect(() => {
+        const nextSlide=()=>{
+            setCurrent(current===length-1 ? 0 : current + 1 )
+        }
+     timeout.current=setTimeout(nextSlide,3000)
+
+     return function(){
+         if(timeout.current){
+             clearTimeout(timeout.current)
+         }
+     }
+    }, [current,length])
+
     const nextSlide=()=>{
+        if(timeout.current){
+            clearTimeout(timeout.current)
+        }
         setCurrent(current===length-1 ? 0 : current + 1 )
-        console.log(current);
     }
 
     const prevSlide=()=>{
+        if(timeout.current){
+            clearTimeout(timeout.current)
+        }
         setCurrent(current=== 0 ? length-1  : current - 1 )
-        console.log(current);
+    }
+
+    if(!Array.isArray(slides )|| slides.length<=0){
+        return null
     }
 
     return (
